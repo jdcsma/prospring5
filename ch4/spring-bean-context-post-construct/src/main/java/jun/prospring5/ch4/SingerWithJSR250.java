17bafcd620b5e3c42775856530b9834f93fe4faf
liujun <jdcsma@163.com>
2019-05-13 14:02:20 +0800

    add spring-bean-context-post-construct example code.

---
+++ ch4/spring-bean-context-post-construct/src/main/java/jun/prospring5/ch4/SingerWithJSR250.java
@@ -0,0 +1,52 @@
+package jun.prospring5.ch4;
+
+import javax.annotation.PostConstruct;
+
+public class SingerWithJSR250 {
+
+    private static final String DEFAULT_NAME = "Eric Clapton";
+    private static final int INVALID_AGE = Integer.MAX_VALUE;
+
+    private String name;
+    private int age = INVALID_AGE;
+
+    @PostConstruct
+    public void init() throws IllegalArgumentException {
+
+        System.out.println("Initializing bean:");
+
+        if (this.name == null) {
+            this.name = DEFAULT_NAME;
+        }
+
+        if (this.age == INVALID_AGE) {
+            throw new IllegalArgumentException(
+                    "You must set the age property of any beans of type " +
+                    SingerWithJSR250.class);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "SingerWithJSR250{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
