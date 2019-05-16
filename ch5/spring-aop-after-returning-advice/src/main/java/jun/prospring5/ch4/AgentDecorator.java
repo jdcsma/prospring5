00ab9919caac0d6e37af362a1de1b13567575a45
liujun <jdcsma@163.com>
2019-05-16 17:29:47 +0800

    add  spring-aop-after-returning-advice example code.

---
+++ ch5/spring-aop-after-returning-advice/src/main/java/jun/prospring5/ch4/AgentDecorator.java
@@ -0,0 +1,17 @@
+package jun.prospring5.ch4;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+public class AgentDecorator implements AfterReturningAdvice {
+
+    @Override
+    public void afterReturning(
+            Object returnValue, Method method,
+            Object[] args, Object target)
+            throws Throwable {
+        System.out.println();
+        System.out.println("After '" + method.getName() + "' have a look see." );
+    }
+}
