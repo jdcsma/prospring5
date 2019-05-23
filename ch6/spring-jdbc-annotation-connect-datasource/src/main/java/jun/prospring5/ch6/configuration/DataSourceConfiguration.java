1e079e2ec4007b0da036d090551a93f9259e1084
liujun <jdcsma@163.com>
2019-05-23 10:56:31 +0800

    add spring-jdbc-annotation-connect-datasource example code.

---
+++ ch6/spring-jdbc-annotation-connect-datasource/src/main/java/jun/prospring5/ch6/configuration/DataSourceConfiguration.java
@@ -0,0 +1,57 @@
+package jun.prospring5.ch6.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@Configuration
+@PropertySource("classpath:jdbc.properties")
+public class DataSourceConfiguration {
+
+    @Value("${databaseDriverClassName}")
+    private String driverClassName;
+
+    @Value("${databaseUrl}")
+    private String url;
+
+    @Value("${databaseUsername}")
+    private String username;
+
+    @Value("${databasePassword}")
+    private String password;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer
+    propertySourcesPlaceholderConfigure() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Lazy
+    @Bean
+    public DataSource dataSource() {
+
+        SimpleDriverDataSource dataSource = null;
+
+        try {
+            dataSource = new SimpleDriverDataSource();
+            @SuppressWarnings("unchecked")
+            Class<? extends Driver> driverClass =
+                    (Class<? extends Driver>) Class.forName(this.driverClassName);
+            dataSource.setDriverClass(driverClass);
+            dataSource.setUrl(this.url);
+            dataSource.setUsername(this.username);
+            dataSource.setPassword(this.password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return dataSource;
+    }
+}
