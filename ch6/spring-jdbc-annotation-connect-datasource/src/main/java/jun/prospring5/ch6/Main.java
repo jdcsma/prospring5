e127980842c98b1d2d7a1a7595e1edc3e0c16186
liujun <jdcsma@163.com>
2019-05-23 11:59:21 +0800

    remove unused code.

--- ch6/spring-jdbc-annotation-connect-datasource/src/main/java/jun/prospring5/ch6/Main.java
+++ ch6/spring-jdbc-annotation-connect-datasource/src/main/java/jun/prospring5/ch6/Main.java
@@ -1,12 +1,6 @@
 package jun.prospring5.ch6;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
-
-    private static Logger logger =
-            LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
