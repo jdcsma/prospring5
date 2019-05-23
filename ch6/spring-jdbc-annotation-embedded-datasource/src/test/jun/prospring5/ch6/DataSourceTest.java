7dee931e19dd3265c5ec57e725136d5548d77ee6
liujun <jdcsma@163.com>
2019-05-23 12:01:55 +0800

    add spring-jdbc-context/annotation-embedded-datasource example code.

---
+++ ch6/spring-jdbc-annotation-embedded-datasource/src/test/jun/prospring5/ch6/DataSourceTest.java
@@ -0,0 +1,50 @@
+package jun.prospring5.ch6;
+
+import jun.prospring5.ch6.configuration.DataSourceConfiguration;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataSourceTest {
+
+    @Test
+    public void dataSourceAnnotation() {
+        AnnotationConfigApplicationContext appContext =
+                new AnnotationConfigApplicationContext(
+                        DataSourceConfiguration.class);
+
+        dataSourceTest(appContext);
+
+        appContext.close();
+    }
+
+    private void dataSourceTest(ApplicationContext appContext) {
+
+        DataSource dataSource = appContext.getBean(
+                "dataSource", DataSource.class);
+
+        Assert.assertNotNull(dataSource);
+
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement =
+                    connection.prepareStatement("SELECT 1");
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int mockVal = resultSet.getInt("1");
+                Assert.assertTrue(mockVal == 1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ch6/spring-jdbc-context-embedded-datasource/src/test/jun/prospring5/ch6/DataSourceTest.java
@@ -0,0 +1,56 @@
+package jun.prospring5.ch6;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataSourceTest {
+
+    @Test
+    public void dataSourceXml() {
+        dataSourceXml("classpath:app-context-xml.xml");
+    }
+
+    public void dataSourceXml(String fileXml) {
+        GenericXmlApplicationContext appContext =
+                new GenericXmlApplicationContext();
+        appContext.load(fileXml);
+        appContext.refresh();
+
+        dataSourceTest(appContext);
+
+        appContext.close();
+    }
+
+    private void dataSourceTest(ApplicationContext appContext) {
+
+        DataSource dataSource = appContext.getBean(
+                "dataSource", DataSource.class);
+
+        Assert.assertNotNull(dataSource);
+
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement =
+                    connection.prepareStatement("SELECT 1");
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int mockVal = resultSet.getInt("1");
+                Assert.assertTrue(mockVal == 1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
