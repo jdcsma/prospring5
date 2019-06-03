775951970386976ba2f34fa5bcd277ee1a63c506
liujun <jdcsma@163.com>
2019-06-03 14:01:22 +0800

    add spring-jpa-hibernate-context-spring-data-crud-repository example code.

---
+++ ch8/spring-jpa-hibernate-context-spring-data-crud-repository/src/main/java/jun/prospring5/ch8/service/implementation/SingerServiceImpl.java
@@ -0,0 +1,67 @@
+package jun.prospring5.ch8.service.implementation;
+
+import jun.prospring5.ch8.entity.Singer;
+import jun.prospring5.ch8.repository.SingerRepository;
+import jun.prospring5.ch8.service.SingerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service("singerService")
+@Repository
+@Transactional
+public class SingerServiceImpl
+        implements SingerService {
+
+    @Autowired
+    private SingerRepository singerRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Singer> findAll() {
+        return newArrayList(singerRepository.findAll());
+    }
+
+    @Override
+    public List<Singer> findAllWithDetails() {
+        return entityManager.createNamedQuery(
+                Singer.FIND_ALL_WITH_DETAILS, Singer.class)
+                .getResultList();
+    }
+
+    @Override
+    public Singer findById(Long id) {
+        Optional<Singer> singer = singerRepository.findById(id);
+        return singer.isPresent() ? singer.get() : null;
+    }
+
+    @Override
+    public List<Singer> findByFirstName(String firstName) {
+        return newArrayList(singerRepository.findByFirstName(firstName));
+    }
+
+    @Override
+    public Singer save(Singer singer) {
+        return singerRepository.save(singer);
+    }
+
+    @Override
+    public void delete(Singer singer) {
+        singerRepository.delete(singer);
+    }
+
+    private static <T> List<T> newArrayList(Iterable<T> iterable) {
+        List<T> singers = new ArrayList<>();
+        iterable.forEach(singers::add);
+        return singers;
+    }
+}
