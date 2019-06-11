5a3e5e8e3ed389eb137c9516f8a7e8e70a1a7af4
liujun <jdcsma@163.com>
2019-06-11 11:48:15 +0800

    add spring-convert-format-context-property-editor example code.

---
+++ ch10/spring-convert-format-context-property-editor/src/test/jun/prospring5/ch10/PropertyEditorTest.java
@@ -0,0 +1,45 @@
+package jun.prospring5.ch10;
+
+import jun.prospring5.ch10.entity.Singer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PropertyEditorTest {
+
+    private static Logger logger =
+            LoggerFactory.getLogger(PropertyEditorTest.class);
+
+    private GenericApplicationContext appContext;
+
+    @Before
+    public void jdbcSetup() {
+
+        GenericXmlApplicationContext context =
+                new GenericXmlApplicationContext();
+        context.load("classpath:app-context-xml.xml");
+        context.refresh();
+
+        appContext = context;
+    }
+
+    @Test
+    public void jdbcTest() {
+
+        Singer singerA = (Singer) appContext.getBean("eric");
+        logger.info("eric info:" + singerA);
+        Singer singerB = (Singer) appContext.getBean("countrySinger");
+        logger.info("john info:" + singerB);
+    }
+
+    @After
+    public void jdbcDown() {
+        if (appContext != null) {
+            appContext.close();
+        }
+    }
+}
